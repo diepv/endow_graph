c18bb9e354ad5415fc736e90b6f7c9eb31f230c2
vdiep <vdiep@mit.edu>
2017-03-24 17:36:29 -0700

    allow origin

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var allowCrossDomain = function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "http://viviandiep.com");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+
+};
+
+app.use(allowCrossDomain);
 app.use('/users', users);
 
 app.get('/reddit',reddit.getThem);
